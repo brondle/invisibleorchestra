9a6381e892c84a9b9068255e17bde50b474360fd
Hayk Mikayelyan <mikahayk@gmail.com>
2018-12-02 22:14:43 -0500

    multiple sensors and LED

--- basic IR/js/app.js
+++ basic IR/js/app.js
@@ -1,7 +1,7 @@
 let serial2;
 let oldNote = 0;
 let synth = new Tone.Synth().toMaster();
-let fromSerial0 = 0, fromSerial1 = 0;
+let fromSerial0 = 0, fromSerial1 = 0, fromSerial2 = 0, fromSerial3 = 0;
 
 function setup() {
 
@@ -21,15 +21,31 @@ function serialEvent() {
 		
 		fromSerial0 = Number(distanceValues[0]);             // get the first item in the array and turn into integer
 		fromSerial1 = Number(distanceValues[1]);
+		fromSerial2 = Number(distanceValues[2]);
+		fromSerial3 = Number(distanceValues[3]);
+
 		
-		console.log("A0:" + fromSerial0 + "     A1:"+fromSerial1);
-		
-		/*
-		if( rawData < 2500 ) {
-			console.log(rawData);
-			synth.triggerAttackRelease( rawData, '8n');
+		if( fromSerial0 < 2500 ) {
+			console.log(fromSerial0);
+			synth.triggerAttackRelease( fromSerial0, '8n');
 		}
-		*/
+		
+		if( fromSerial1 < 2500 ) {
+			console.log(fromSerial1);
+			synth.triggerAttackRelease( fromSerial1, '8n');
+		}
+
+		if( fromSerial2 < 2500 ) {
+			console.log(fromSerial2);
+			synth.triggerAttackRelease( fromSerial2, '8n');
+		}		
+		
+		if( fromSerial3 < 2500 ) {
+			console.log(fromSerial3);
+			synth.triggerAttackRelease( fromSerial3, '8n');
+		}		
+		
+
 
 	}
 	
